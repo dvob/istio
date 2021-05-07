1b745cbe89346814d53256226b2601933e25cc76
David Schneider <schneider@puzzle.ch>
2021-05-07 10:54:29 +0200

    Do not cache envoy state

--- pilot/cmd/pilot-agent/status/ready/probe.go
+++ pilot/cmd/pilot-agent/status/ready/probe.go
@@ -71,18 +71,8 @@ func (p *Probe) checkConfigStatus() error {
 
 // isEnvoyReady checks to ensure that Envoy is in the LIVE state and workers have started.
 func (p *Probe) isEnvoyReady() error {
-	// If Envoy is ready atleast once i.e. server state is LIVE and workers
-	// have started, they will not go back in the life time of Envoy process.
-	// They will only change at hot restart or health check fails. Since Istio
-	// does not use both of them, it is safe to cache this value. Since the
-	// actual readiness probe goes via Envoy it ensures that Envoy is actively
-	// serving traffic and we can rely on that.
-	if p.atleastOnceReady {
-		return nil
-	}
-
 	err := checkEnvoyStats(p.LocalHostAddr, p.AdminPort)
-	if err == nil {
+	if err == nil && !p.atleastOnceReady {
 		metrics.RecordStartupTime()
 		p.atleastOnceReady = true
 	}
